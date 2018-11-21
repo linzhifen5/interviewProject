a9511e2d27e137ce6b9b4b2920bbe28e0ea25cf8
zhl <1786524543@qq.com>
2018-11-21 19:33:00 +0800

    添加插件以后

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import './assets/scss/index.scss'
+import './assets/scss/index.scss'
 import fastclick from 'fastclick';
 
 fastclick.attach(document.body);
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
-  // components: { App },
-  // template: '<App/>'
+  // render: h => h(App)
+  components: { App },
+  template: '<App/>'
 })
