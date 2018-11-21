71d9f83a194d586b88966d7b396a5e573bebd387
zhl <1786524543@qq.com>
2018-11-21 18:18:30 +0800

    报错提交

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// import './assets/scss/index.scss'
+import './assets/scss/index.scss'
+import fastclick from 'fastclick';
+
+fastclick.attach(document.body);
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
+  // components: { App },
+  // template: '<App/>'
 })
