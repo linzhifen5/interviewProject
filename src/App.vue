a7b465e922ffd3f3e62ee443f493d2865cea6525
zhl <1786524543@qq.com>
2018-11-22 17:50:35 +0800

    1122 代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <div id="app">
-    首页123
+  <div id="app" class="g-container">
+    <div class="g-view-container">
+      <div class="g-footer-container">
+        <c-tabbar/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CTabbar from 'components/tabbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    CTabbar
+  }
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/components/tabbar/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="g-tabbar">
+    <router-link class="g-tabbar-item" to="/home">
+      <i class="icofont icon-home">
+        <span>首页</span>
+      </i>
+    </router-link>
+    <router-link class="g-tabbar-item" to="/category">
+      <i class="icofont icon-category">
+        <span>分类页</span>
+      </i>
+    </router-link>
+    <router-link class="g-tabbar-item" to="/cart">
+      <i class="icofont icon-cart">
+        <span>购物车</span>
+      </i>
+    </router-link>
+    <router-link class="g-tabbar-item" to="/personal">
+      <i class="icofont icon-personal">
+        <span>个人中心</span>
+      </i>
+    </router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CTabbar'
+  }
+</script>
